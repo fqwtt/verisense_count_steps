bc2e3bfff3b45d83fdd23dd0c085b7b18c9fac56
fqwtt <1748495562@qq.com>
2023-08-30 23:13:33 +0800

    上传计算分钟步数代码

--- cal_steps_per_min.R
+++ cal_steps_per_min.R
@@ -3,15 +3,15 @@ load("~/Desktop/verisense_count_steps/output/output_input/meta/basic/meta_480001
 timestamp_per_5_secs <- M[["metashort"]][["timestamp"]]
 steps_per_5_secs <- M[["metashort"]][["step_count"]]
 num_minutes = length(steps_per_5_secs) %/% 12
-timestamp_per_mins = rep('', num_minutes)
-steps_per_mins = rep(0, num_minutes)
+timestamp_per_min = rep('', num_minutes)
+steps_per_min = rep(0, num_minutes)
 for(i in 1:num_minutes){
-  timestamp_per_mins[i] = timestamp_per_5_secs[(i -1) * 12 + 1]
+  timestamp_per_min[i] = timestamp_per_5_secs[(i -1) * 12 + 1]
   for(j in 1:12){
-    steps_per_mins[i] = steps_per_mins[i] + steps_per_5_secs[(i - 1) * 12 + j]
+    steps_per_min[i] = steps_per_min[i] + steps_per_5_secs[(i - 1) * 12 + j]
   }
 }
 
 file_name = "~/Desktop/verisense_count_steps/steps_per_min.csv"
-my_data <- data.frame(timestamp_per_mins, steps_per_mins)
+my_data <- data.frame(timestamp_per_min, steps_per_min)
 write.csv(my_data, file = file_name, row.names = FALSE)
